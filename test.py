f5c62251d88d2f53a29f8e76482e8a1d4c14b71d
Sharki1234 <thanishkavenkata28@gmail.com>
2025-02-21 19:00:20 +0000

    123

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("hello")
+class Student:
+    name = "Stephanie"
+    def test(self):
+        print(15)
+
+
+person = Student()
+p2 = Student()
+p2.name = "radish"
+print(person.name)
+p2.test()
