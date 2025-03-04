513167a445fad22559c2eb9579d7fe168e847a4c
Sharki1234 <thanishkavenkata28@gmail.com>
2025-03-04 20:58:29 +0000

    alomost done add the repeats in add didn't really understand it so made some changes hope teacher doesn't mind

--- cart.py
+++ cart.py
@@ -7,36 +7,44 @@ class Product:
 
         
 class User:
-    def __init__(self):
-        self.cart = []
-        self.cartnames = []
+    cart = []
+    cartnames = []
+    def add(self):
         useradd = input("what product do you want to add")
         b = Product()
         price = b.find(useradd)
         self.cart.append(price)
         self.cartnames.append(useradd)
+    def view(self):
+        orders = 0
+        order_record = []
+        view = input("do you want to view your products ").lower()
+        if view == "yes":
+            print(" ".join(self.cartnames))
+            place = input("are you ready to order?").lower()
+            if place == "yes":
+                orders += 1
+                order_name = (f"order {orders}){len(user.cartnames)} items")
+                order_record.append(order_name)
+            else:
+                print("go back to the menu")
+        else:
+            print("go back to menu")
 
     
-
-class Cart:
-    user = User()
-    orders = 0
-    order_record = []
-    view = input("do you want to view your products ").lower()
-    if view == "yes":
-        print(" ".join(user.cartnames))
-        place = input("are you ready to order?").lower()
-        if place == "yes":
-            orders += 1
-            order_name = (f"order {orders}){len(user.cartnames)} items")
-            order_record.append(order_name)
-        else:
-            print("go back to the menu")
-    else:
-        print("go back to menu")
     
         
 class Orders:
     def view_orders(self):
         x = Cart()
         print(''.join(x.order_record))
+
+while True:
+    menu = input("what do you wnat to do?").lower()
+    action = User()
+    if menu == "add":
+        action.add()
+    elif menu == "check cart":
+        action.view()
+    elif menu == "orders":
+        action = Orders()
