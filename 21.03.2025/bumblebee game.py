9a9ca017911cef000bd45b99301c0b6dd6918f2a
Sharki1234 <thanishkavenkata28@gmail.com>
2025-03-22 17:41:35 +0000

    hw

---
+++ 21.03.2025/bumblebee game.py
@@ -0,0 +1,36 @@
+import pgzrun
+import random
+
+WIDTH = 500
+HEIGHT = 500
+score = 0
+message = ""
+mouse = Actor('jerry')
+cheese = Actor('cheeze')
+cheese.x = 250
+cheese.y = 250
+def draw():
+    screen.blit("clouds acc",(0,0))
+    mouse.draw()
+    cheese.draw()
+    message = (f"your score is: {score}")
+    screen.draw.text(message,center = (50,20),fontsize = 20)
+
+def update():
+    global score,message
+    if keyboard.left:
+        mouse.x -=10
+    elif keyboard.right:
+        mouse.x +=10
+    elif keyboard.up:
+        mouse.y -=10
+    elif keyboard.down:
+        mouse.y +=10
+    
+    if mouse.colliderect(cheese):
+        score+=1
+        cheese.x = random.randint(0,500)
+        cheese.y = random.randint(0,500)
+
+
+pgzrun.go()
