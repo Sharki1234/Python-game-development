bd98c9ac502a29774eaecadbfd38c5394355ef02
Sharki1234 <thanishkavenkata28@gmail.com>
2025-04-09 20:49:29 +0100

    not finished but charging up so have to save

--- treasure_hunt.py
+++ treasure_hunt.py
@@ -3,24 +3,41 @@ import random
 board = []
 def board_creation():
     for i in range(3):
-        row = []
-        for i in range(3):
-            row .append("x ")
+      row = []
+      for i in range(3):
+        row .append("x ")
         board.append(row)
-        print(''.join(row))
+      print(''.join(board[i]))
 
 #randomise coordinates where treasure is - part of list
-def coordinates():
+
+def game():
+    flag = False
+    tries = 3
     board_creation()
     y = random.randint(0,2)
     x = random.randint(0,2)
-    return x,y
-#check if right
-def check():
-    x,y = coordinates()
     user_row = int(input("What row?"))
     user_column = int(input("What column?"))
-    
-# tries
-#hints up or down or left
+    while not flag:
+        board_creation()
+        if user_row == x and user_column == y:
+            print("CORRECT")
+            flag == True
+        if user_column > y:
+            print("Hint:go up")
+            tries -= 1
+        if user_column < y:
+            print("Hint:go down")
+            tries -=1
+        if user_row > x:
+            print("Hint:go left")
+            tries -=1
+        if user_row < x:
+            print("Hint:go right")
+            tries -=1
 
+
+        # tries
+        #hints up or down or left
+game()
