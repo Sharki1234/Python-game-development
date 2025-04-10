78f0092199fa5a936dac99e085edc20aa2c88c13
Sharki1234 <thanishkavenkata28@gmail.com>
2025-04-10 09:43:57 +0100

    finished i think but had no use for 2d lists

--- treasure_hunt.py
+++ treasure_hunt.py
@@ -3,38 +3,36 @@ import random
 board = []
 def board_creation():
     for i in range(3):
-      row = []
-      for i in range(3):
-        row .append("x ")
-        board.append(row)
-      print(''.join(board[i]))
+        row = []
+        for i in range(3):
+            row .append("x ")
+        print(''.join(row))
 
 #randomise coordinates where treasure is - part of list
 
 def game():
     flag = False
     tries = 3
-    board_creation()
-    y = random.randint(0,2)
-    x = random.randint(0,2)
-    user_row = int(input("What row?"))
-    user_column = int(input("What column?"))
+    y = random.randint(1,3)
+    x = random.randint(1,3)
     while not flag:
         board_creation()
+        user_row = int(input("What row?"))
+        user_column = int(input("What column?"))
         if user_row == x and user_column == y:
             print("CORRECT")
             flag == True
         if user_column > y:
-            print("Hint:go up")
+            print("Hint:go left")
             tries -= 1
         if user_column < y:
-            print("Hint:go down")
+            print("Hint:go right")
             tries -=1
         if user_row > x:
-            print("Hint:go left")
+            print("Hint:go up")
             tries -=1
         if user_row < x:
-            print("Hint:go right")
+            print("Hint:go down")
             tries -=1
 
 
