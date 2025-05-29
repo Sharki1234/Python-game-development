1a44f7c6a5b14e1c4668c3c1e7469c5b23c3212a
Sharki1234 <thanishkavenkata28@gmail.com>
2025-05-29 17:06:31 +0100

    not done

--- 23.05.2025/quiz.py
+++ 23.05.2025/quiz.py
@@ -1,5 +1,12 @@
+import pgzrun
+import random
+WIDTH = 500
+HEIGHT = 410
+gap = 10
 full = [] 
-    
+recs = []
+level = 0
+
 def get_questions():
     file = open("23.05.2025\practice.txt","r")
     
@@ -21,7 +28,56 @@ def get_questions():
             "answer":options[answer-1]
         }
         full.append(dict)
+    random.shuffle(full)
 
     print(full)
     file.close()
-get_questions
+get_questions()
+
+def draw():
+    x = (WIDTH - (gap*3))/2
+    y = (HEIGHT - (gap*4))/3
+    p = gap + (x/2)
+    n = (y+(gap*2))+(y/2)
+    screen.fill(color = (150,238,137))  
+    rect = Rect((0,0),(x,y))
+    rect.center = (p,n)
+    screen.draw.filled_rect(rect,(8,84,47))#drawing teh rectangles
+
+    rect2 = Rect((0,0),(x,y))
+    rect2.center = (WIDTH-p,n)
+    screen.draw.filled_rect(rect2,(8,84,47))
+
+    rect3 = Rect((0,0),(x,y))
+    rect3.center =(WIDTH-p,n+(y)+gap)
+    screen.draw.filled_rect(rect3,(8,84,47))
+
+    rect4 = Rect((0,0),(x,y))
+    rect4.center = (p,n+(y)+gap)
+    screen.draw.filled_rect(rect4,(8,84,47))
+
+    qbox = Rect((0,0),((2*x)+gap,y))
+    qbox.center = ((gap*1.5)+x,gap+(y/2))
+    screen.draw.filled_rect(qbox,(1,45,33))
+
+    recs.append(rect)#adding to list
+    recs.append(rect2)
+    recs.append(rect3)
+    recs.append(rect4)
+    screen.draw.text((full[level]["question"]),(gap+10,gap+(y/2)),fontsize = 20)#putting question into teh box
+    screen.draw.text(full[level]["options"][0],((gap+10),n),fontsize=20)#printing options
+
+def on_mouse_click(pos):
+    global level
+    for i in range(recs):
+        if recs[i].collidepoint(pos):
+            print(i)
+            print(pos)
+            if i == full[level]["answer"]:
+                level+=1#if the index of the rectangle that you have clicked is equal to teh index of the answer 
+                #you will move to teh next level
+            else:
+                print("hello")
+        
+
+pgzrun.go()
