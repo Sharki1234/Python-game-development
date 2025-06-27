6af36342643d5246aacc81640b18a288c8268712
Sharki1234 <thanishkavenkata28@gmail.com>
2025-06-27 19:02:32 +0100

    abc

---
+++ pygame/circle.py
@@ -0,0 +1,33 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((500,500))
+screen.fill((255,255,255))
+
+start = True
+
+class Circle():
+    def __init__(self,colour,center,radius):
+        self.colour = colour
+        self.radius = radius
+        self.center = center
+        self.scrn = screen
+    def draw(self):
+        pygame.draw.circle(self.scrn,self.colour,self.center,self.radius)
+    def grow(self):
+        self.radius +=10
+        pygame.draw.circle(self.scrn,self.colour,self.center,self.radius)
+
+
+
+while start:
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            start = False
+            break
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            tiny_circle = Circle((0,0,255),(250,250),40)
+            tiny_circle.draw()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            tiny_circle.grow()
+        pygame.display.update()
