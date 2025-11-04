af3dac757707e2d4d8d69ee48e7db594203a970d
Sharki1234 <thanishkavenkata28@gmail.com>
2025-11-04 18:55:10 +0000

    need to finish calc

--- pygame/calc.py
+++ pygame/calc.py
@@ -8,19 +8,18 @@ class Calc:
     def __init__(self):
         self.buttons = []
         self.board()
-      
+        self.button_values = [[7,8,9,"/"],[4,5,6,"X"],[3,2,1,"-"],["+",0,"=","AC"]]
+        self.equation = []
     def board(self):
         gap = 50
         self.answerboard = pygame.Rect((gap),(gap),WIDTH-(gap*2),(gap*3))
         buttonscreen_width = gap*10
         buttonscreen_height = gap*6
-        buttonscreen = pygame.Rect(gap,gap*5,buttonscreen_width,buttonscreen_height)
         button_width = (buttonscreen_width-(gap*3))/4
         button_height = (buttonscreen_height-((gap/2)*3))/4
         for n in range (4):
             button_row = []
             for i in range(4):
-                #left =  gap + ((WIDTH - (gap * 5)) / 4 + gap) * i
                 button_left = (button_width*i)+(gap*(i+1))
                 button_top = (button_height*n)+((gap/2)*(n+1))+(gap*4)
                 button = pygame.Rect(button_left,button_top,button_width,button_height)
@@ -31,6 +30,20 @@ class Calc:
             for button in button_row:
                 pygame.draw.rect(screen,(17,130,59),button,0,4)
         pygame.draw.rect(screen,(72,191,83),self.answerboard,0,4)
+
+        for i in range(len(self.buttons)):
+            for n in range(len(self.buttons[i])):
+                font = pygame.font.SysFont("Times New Roman",20)
+                t = self.button_values[i][n]
+                text = font.render(str(t),1,(0,0,0))
+                screen.blit(text,(self.buttons[i][n].center))
+    def click(self):
+        pos = pygame.mouse.get_pos()
+        for i in range(len(self.buttons)):
+            for n in range(len(self.buttons[i])):
+                if self.buttons[i][n].collidepoint(pos):
+                    self.equation.append(self.button_values[i][n])
+                    print(self.button_values[i][n])
 calc = Calc()  
 while True:
     screen.fill((165,240,154))
@@ -38,4 +51,6 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            calc.click()
     pygame.display.update()
