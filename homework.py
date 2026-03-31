b854e97e5633d83c2ac4bc73a0aed082245de814
Shkarki1234 <thanishkavenkata28@gmail.com>
2026-03-31 17:04:33 +0100

    Lesson 9 homework 1

--- homework.py
+++ homework.py
@@ -1,46 +1,9 @@
-import pgzrun
-from random import randint
-WIDTH = 500
-HEIGHT = 500
-TITLE = "INFINITY GAUNTLET HOMEWORK"
-
-infinity_gauntlet = Actor("infinity_gauntlet")
-infinity_gauntlet.pos=(randint(0,500),250)
-
-def draw():
-    screen.clear()
-    infinity_gauntlet.draw()
-
-def update():
-    if keyboard.left:
-        infinity_gauntlet.x=infinity_gauntlet.x-10
-        if infinity_gauntlet.x<=0:
-            infinity_gauntlet.x=0
-    if keyboard.right:
-        infinity_gauntlet.x=infinity_gauntlet.x+10 
-        if infinity_gauntlet.x>=500:
-            infinity_gauntlet.x=500    
-    if keyboard.up:
-        infinity_gauntlet.y=infinity_gauntlet.y-10 
-        if infinity_gauntlet.y>=500:
-            infinity_gauntlet.y=500 
-    if keyboard.down:
-        infinity_gauntlet.y=infinity_gauntlet.y+10 
-        if infinity_gauntlet.y>=500:
-            infinity_gauntlet.y=500              
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-pgzrun.go()            
+import random
+Number = random.randint(1,21)
+Question = int(input("Please guess the number"))
+if Question < Number:
+    print("too low")
+elif Question > Number:
+    print("too high")
+elif Question == Number:
+    print("Correct!You guessed the number")
