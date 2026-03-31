adc5a5e9d59ddda9e4052b26f5b676035c99e964
Shkarki1234 <thanishkavenkata28@gmail.com>
2026-03-31 16:58:35 +0100

    challenge1
    first python challenge

---
+++ challenge1.py
@@ -0,0 +1,11 @@
+challist = []
+for i in range(5):
+    element = input("Please enter something you wish to add to the list above")
+    challist.append(element)
+print(challist)
+mid = len(challist)//2
+print(challist[mid])
+left = 0
+right = -1
+print(challist[left])
+print(challist[right])
